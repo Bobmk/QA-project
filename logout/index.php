e51755c73bd9872f7471d59d51c3c5f1939ed45f
Md. Mushiar Khan <mushiarkhan@gmail.com>
2015-05-12 22:42:55 +0530

    modified logout page

--- logout/index.php
+++ logout/index.php
@@ -1,7 +1,8 @@
 <?php
 require_once "../assets/components/php/functions.php" ;
 session_start();
-$_SESSION=array();
+
+session_unset();
 
 if(isset($_COOKIE[session_name()])){
 	setcookie(session_name(),null,time()-42000);
